dc15fb9727c22f7e0f0a078f10743c44f9dbad2b
Gauthamkumar <a.r.gauthamkumar@gmail.com>
2022-09-15 16:27:37 +0530

    added error notifications

--- index.js
+++ index.js
@@ -2,12 +2,23 @@ let users = [];
 let userName = document.getElementById("username").value;
 let password = document.getElementById("password").value;
 
-
-function onLoginClicked(){
-  let userCredentials = {
-    userName : userName,
-    password : password
-  };
-  users.push(userCredentials);
-  console.log(users);
+function onLoginClicked(event) {
+  event.preventDefault();
+  if (userName.value !== undefined && password.value !== undefined) {
+    userArray.push({
+      userName: userName.value,
+      password: password.value,
+    });
+    localStorage.setItem("users", JSON.stringify(userArray));
+  } else if (userName.value === undefined) {
+    userName.textContent = "Enter Username";
+  } else if (password.value === undefined) {
+    password.textContent = "Enter Password";
+  } else if (userName.value === undefined && password.value === undefined) {
+    userName.textContent = "Enter Username";
+    password.textContent = "Enter Password";
+  } else {
+    userName.textContent = "";
+    password.textContent = "";
+  }
 }
